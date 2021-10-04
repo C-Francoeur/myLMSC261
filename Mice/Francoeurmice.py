d7525bef9faafc243d42860463ffa9578e2f5fe5
C. Francoeur <90637766+C-Francoeur@users.noreply.github.com>
2021-10-04 19:58:01 -0400

    Added second solution

--- Mice/Francoeurmice.py
+++ Mice/Francoeurmice.py
@@ -1,6 +1,14 @@
 x = 4
 y = 3
+
 print(x - y, "	Blind", "	       Mice", sep="\n")
+will get you
 3
 	Blind
 	       Mice
+
+Another option
+print(x - y, "\n\tBlind\n\t\tMice")
+3
+	Blind
+            Mice
