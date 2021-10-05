64c161028a8fd64cc9de76948aac20ca601f9994
C. Francoeur <90637766+C-Francoeur@users.noreply.github.com>
2021-10-04 20:06:50 -0400

    updates

--- Mice/Francoeurmice.py
+++ Mice/Francoeurmice.py
@@ -1,14 +1,8 @@
 x = 4
 y = 3
 
-print(x - y, "	Blind", "	       Mice", sep="\n")
-will get you
-3
-	Blind
-	       Mice
-
-Another option
 print(x - y, "\n\tBlind\n\t\tMice")
+
 3
 	Blind
             Mice
