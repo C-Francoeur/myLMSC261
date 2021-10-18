fc46f7aa568f3e3b48a4bc7300a40ebe808eaa90
C. Francoeur <90637766+C-Francoeur@users.noreply.github.com>
2021-10-18 19:05:45 -0400

    more programing/code

--- Pyramid/FrancoeurPyramid.py
+++ Pyramid/FrancoeurPyramid.py
@@ -2,7 +2,18 @@ stacks = range(1,8)
 
 if stacks == 1:
     print("#")
-if stacks == 2:
+elif stacks == 2:
     print(" #/n##")
-if stacks == 3:
+elif stacks == 3:
     print("  #/n ##/n###")
+elif stacks == 4:
+    print("   #/n  ##/n ###/n####")
+elif stacks == 5:
+    print("    #/n   ##/n  ###/n ####/n#####")
+elif stacks == 6:
+    print("     #/n    ##/n   ###/n  ####/n #####/n######")
+elif stacks == 7:
+    print("      #/n     ##/n    ###/n   ####/n  #####/n #####/n######")
+else stacks == 8:
+    print("       #/n      ##/n     ###/n    ####/n   #####/n  ######/n #######/n########")
+    
