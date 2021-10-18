422ab2688fbbdb3edf753be79bd7da8ef3054154
C. Francoeur <90637766+C-Francoeur@users.noreply.github.com>
2021-10-18 18:52:44 -0400

    testing

--- Pyramid/FrancoeurPyramid.py
+++ Pyramid/FrancoeurPyramid.py
@@ -1,8 +1,8 @@
 stacks = range(1,8)
 
-if stacks = 1:
+if stacks == 1:
     print("#")
-if stacks = 2:
-    print("#/n##")
-
-    
+if stacks == 2:
+    print(" #/n##")
+if stacks == 3:
+    print("  #/n ##/n###")
