9cb57c9b7402485813a54f8449dce991b237ce0b
C. Francoeur <90637766+C-Francoeur@users.noreply.github.com>
2021-10-19 18:49:31 -0400

    Updated and fixed code

--- Pyramid/FrancoeurPyramid.py
+++ Pyramid/FrancoeurPyramid.py
@@ -1,18 +1,22 @@
-stacks = range(1,8)
+stacks = int(input("Enter an integer from one to eight to determine pyramid height: "))
 
-if stacks == 1:
+if stacks <= 0 :
+    print("Invalid integer")
+elif stacks == 1 :
     print("#")
-elif stacks == 2:
-    print(" #/n##")
-elif stacks == 3:
-    print("  #/n ##/n###")
-elif stacks == 4:
-    print("   #/n  ##/n ###/n####")
-elif stacks == 5:
-    print("    #/n   ##/n  ###/n ####/n#####")
-elif stacks == 6:
-    print("     #/n    ##/n   ###/n  ####/n #####/n######")
-elif stacks == 7:
-    print("      #/n     ##/n    ###/n   ####/n  #####/n #####/n######")
-elif stacks == 8:
-    print("       #/n      ##/n     ###/n    ####/n   #####/n  ######/n #######/n########")
+elif stacks == 2 :
+    print(" #\n##")
+elif stacks == 3 :
+    print("  #\n ##\n###")
+elif stacks == 4 :
+    print("   #\n  ##\n ###\n####")
+elif stacks == 5 :
+    print("    #\n   ##\n  ###\n ####\n#####")
+elif stacks == 6 :
+    print("     #\n    ##\n   ###\n  ####\n #####\n######")
+elif stacks == 7 :
+    print("      #\n     ##\n    ###\n   ####\n  #####\n ######\n#######")
+elif stacks == 8 :
+    print("       #\n      ##\n     ###\n    ####\n   #####\n  ######\n #######\n########")
+elif stacks >= 9 :
+    print("Integer invalid")
