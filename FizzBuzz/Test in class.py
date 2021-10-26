2139c0421c26e67a424261bfbf0f5c0544630a08
C. Francoeur <90637766+C-Francoeur@users.noreply.github.com>
2021-10-25 20:27:08 -0400

    FizzBuzz

---
+++ FizzBuzz/Test in class.py
@@ -0,0 +1,19 @@
+def func(count):
+    for i in range(count + 1):
+        print(f"{count - i}")
+
+count = 100:
+    for x % 3 == 0 and x % 5 == 0
+        print("FizzBuzz")
+    for x % 3 == 0
+        print("Fizz")
+    for x % 5 == 0 
+        print("Buzz")
+
+        
+    
+
+
+        
+
+
